69b8da7163380ddcbaeab778bc91a306aadb4d44
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-08-14 18:31:22 -0300

    Project created

---
+++ backend/src/main/java/com/montes/desafio_conseiller_weega/DesafioConseillerWeegaApplication.java
@@ -0,0 +1,13 @@
+package com.montes.desafio_conseiller_weega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioConseillerWeegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioConseillerWeegaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/montes/desafio_conseiller_weega/DesafioConseillerWeegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montes.desafio_conseiller_weega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioConseillerWeegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
